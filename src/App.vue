44862fedd688a92c5dbfdaf5de205b7f50412f67
mwp-git <mortenwilliampetersen@gmail.com>
2021-03-11 16:02:48 +0100

    vue git test

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  
+  <PostComponent/>
+</template>
+
+
+<script>
+import PostComponent from './components/PostComponent.vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PostComponent
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/PostComponent.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div>
+    <h1>Submit data to API</h1>
+
+    <input placeholder="Username" v-model="user.username"><br><br>
+    <input placeholder="Password" v-model="user.password"><br><br>
+    <button v-on:click="login" >Post</button>
+
+  </div>
+
+</template>
+
+<script>
+
+import axios from 'axios'
+
+export default{
+  name:"PostComponent",
+  data(){
+    return{
+      user:{
+        username:null,
+        password:null
+      }
+    }
+  },
+
+  methods:{
+    async login(e){
+      const res = await axios.post('https://backend.mama.sh/api/login', this.user);
+      const tkn = await res.json();
+      }
+    }
+}
+</script>
